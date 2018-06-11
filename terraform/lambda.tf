ab98f43919f0f6e453c22196c2e91d9b5538b056
Donna Malayeri <lindydonna@users.noreply.github.com>
2018-06-11 15:17:37 -0700

    Clean up examples

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,30 +3,31 @@ provider "aws" {
 }
 
 data "archive_file" "lambda" {
-  type = "zip"
-  source_file = "main.js"
+  type        = "zip"
+  source_file = "app.js"
   output_path = "lambda.zip"
 }
 
 resource "aws_lambda_function" "example" {
-  filename = "${data.archive_file.lambda.output_path}"
+  filename      = "${data.archive_file.lambda.output_path}"
   function_name = "ServerlessExample"
 
-  # "main" is the filename within the zip file (main.js) and "handler"
+  # "app" is the filename within the zip file (app.js) and "handler"
   # is the name of the property under which the handler function was
   # exported in that file.
-  handler = "main.handler"
+  handler = "app.handler"
+
   runtime = "nodejs6.10"
 
-  role = "${aws_iam_role.lambda_exec.arn}"
+  role             = "${aws_iam_role.lambda_exec.arn}"
   source_code_hash = "${base64sha256(file("${data.archive_file.lambda.output_path}"))}"
-  publish = true
+  publish          = true
 
   environment {
     variables = {
       DYNAMODB_TABLE = "${aws_dynamodb_table.dynamo-table.id}"
     }
-  }  
+  }
 }
 
 # IAM role which dictates what other AWS services the Lambda function
@@ -52,9 +53,10 @@ EOF
 }
 
 resource "aws_iam_role_policy" "lambda-dynamo-role" {
-    name = "dynamo-policy"
-    role = "${aws_iam_role.lambda_exec.id}"
-    policy = <<EOF
+  name = "dynamo-policy"
+  role = "${aws_iam_role.lambda_exec.id}"
+
+  policy = <<EOF
 {
     "Version": "2008-10-17",
     "Statement": [
@@ -67,56 +69,6 @@ resource "aws_iam_role_policy" "lambda-dynamo-role" {
     ]
 }
 EOF
-}
-
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.example.invoke_arn}"
-}
-
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.example.invoke_arn}"
-}
-
-resource "aws_api_gateway_deployment" "example" {
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  stage_name  = "test"
 }
 
 resource "aws_lambda_permission" "apigw" {
